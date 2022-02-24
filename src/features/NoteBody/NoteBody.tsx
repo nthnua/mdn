5ef48050108881593a2be402c517d07ee888d471
nthnua <97590341+nthnua@users.noreply.github.com>
2022-02-24 16:55:13 +0530

    Working markdown parsing

--- src/features/NoteBody/NoteBody.tsx
+++ src/features/NoteBody/NoteBody.tsx
@@ -1,5 +1,6 @@
-import { Flex, Heading } from '@chakra-ui/react'
-import { marked } from 'marked'
+import { Flex } from '@chakra-ui/react'
+import ChakraUIRenderer from 'chakra-ui-markdown-renderer'
+import ReactMarkdown from 'react-markdown'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../app/store'
 
@@ -7,16 +8,14 @@ export default function NoteBody ({ noteId }: {noteId: string}): JSX.Element {
   const noteContent = useSelector((state: RootState) => {
     return state.sidebar.notes.find(note => note.name === noteId)?.content
   })
-  const markedContent = marked.parse(noteContent ?? '')
-  console.log(markedContent)
   return (
     <Flex
-      bg='blue.200' flexGrow='1' minW={{
+      bg='blue.200' flexGrow='1' flexDir='column' minW={{
         base: 'full',
         md: '50%'
       }} rounded='lg' p='4'
     >
-      <div dangerouslySetInnerHTML={{ __html: markedContent }} />
+      <ReactMarkdown components={ChakraUIRenderer()}>{noteContent ?? ''}</ReactMarkdown>
     </Flex>
   )
 }
